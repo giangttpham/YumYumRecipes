81b7898582df0502f59db44a6d328c79704bde9d
Giang Pham <giangttpham@gmail.com>
2014-11-26 20:20:26 -0800

    Initial Commit

---
+++ YumYumRecipes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YumYumRecipes
+//
+//  Created by Tra` Beo' on 11/26/14.
+//  Copyright (c) 2014 Giang Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YumYumRecipes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YumYumRecipes
+//
+//  Created by Tra` Beo' on 11/26/14.
+//  Copyright (c) 2014 Giang Pham. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
